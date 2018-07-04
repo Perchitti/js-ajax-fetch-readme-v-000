59e37f4a096d68113bda22f220551de9afe95605
Lauren Perchitti <laurenperchitti@gmail.com>
2018-07-04 19:41:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const app = "I don't do much.";
 
 
-  const token = 'f89f5a08939186a09a19957da0172763998957db'
-  fetch('https://api.github.com/user/repos', {
-    headers: {
-      Authorization: `token ${token}`
-    }
-  }).then(res => res.json()).then(json => console.log(json));
+fetch('https://api.github.com/user/repos').
+  then(res => res.json()).
+  then(json => console.log(json))
