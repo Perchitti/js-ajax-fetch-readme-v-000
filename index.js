4b24e07e9b9649a9c0ba13782fb6cb659be67b92
Lauren Perchitti <laurenperchitti@gmail.com>
2018-07-04 19:44:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ fetch('https://api.github.com/repos/jquery/jquery/commits')
   .then(res => res.json())
   .then(json => console.log(json));
   
-  const token = 'f89f5a08939186a09a19957da0172763998957db'
+  const token = 'd19cfb6d5234a862374020e3431c8d540962fd10'
   fetch('https://api.github.com/user/repos', {
     headers: {
       Authorization: `token ${token}`
