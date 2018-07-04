ff16021d1b03e0355252548e93ae6001fac15506
Lauren Perchitti <laurenperchitti@gmail.com>
2018-07-04 19:42:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much.";
 
 
-fetch('https://api.github.com/user/repos').
+fetch('https://api.github.com/perchitti/repos').
   then(res => res.json()).
   then(json => console.log(json))
